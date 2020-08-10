e5c5d47a362e0177d987b27906c9c2d46b5d8e16
Veronica Bernier <veronica.bernier@upr.edu>
2020-08-09 21:32:42 -0400

    terrain, sky box, splash scene, music, player ship, circuit, movement

---
+++ Assets/PlayerShip.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerShip : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 15f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 15f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yMinRange = -5f;
+    [Tooltip("In m")] [SerializeField] float yMaxRange = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyThrowMovement();
+    }
+
+    private void ApplyThrowMovement()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime; //in frame, if delta(the change) is longer, we need to move further
+        float rawNewXPos = transform.localPosition.x + xOffset;
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(Mathf.Clamp(rawNewXPos, -xRange, xRange),
+            Mathf.Clamp(rawNewYPos, yMinRange, yMaxRange), transform.localPosition.z); //dont change z
+    }
+}
