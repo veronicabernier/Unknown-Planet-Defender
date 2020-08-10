a37351f31168a14725a71706735bf461d155f136
Veronica Bernier <veronica.bernier@upr.edu>
2020-08-09 22:12:11 -0400

    rotation

--- Assets/PlayerShip.cs
+++ Assets/PlayerShip.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -9,8 +10,16 @@ public class PlayerShip : MonoBehaviour
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 15f;
     [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 15f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
-    [Tooltip("In m")] [SerializeField] float yMinRange = -5f;
-    [Tooltip("In m")] [SerializeField] float yMaxRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yMinRange = -3f;
+    [Tooltip("In m")] [SerializeField] float yMaxRange = 3f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +31,31 @@ public class PlayerShip : MonoBehaviour
     void Update()
     {
         ApplyThrowMovement();
+        ApplyRotation();
     }
 
     private void ApplyThrowMovement()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime; //in frame, if delta(the change) is longer, we need to move further
         float rawNewXPos = transform.localPosition.x + xOffset;
 
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
         float rawNewYPos = transform.localPosition.y + yOffset;
 
         transform.localPosition = new Vector3(Mathf.Clamp(rawNewXPos, -xRange, xRange),
             Mathf.Clamp(rawNewYPos, yMinRange, yMaxRange), transform.localPosition.z); //dont change z
+    }
+
+    private void ApplyRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor; //due to position
+        float roll = xThrow  * controlRollFactor; //due to control
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
