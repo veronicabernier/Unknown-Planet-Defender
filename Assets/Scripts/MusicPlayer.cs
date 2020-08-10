5cafbf5cbe77742406327258822b961067bd0c17
Veronica Bernier <veronica.bernier@upr.edu>
2020-08-10 17:24:04 -0400

    SceneLoader and tidying

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] float delayLoadTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //wait and load next scene
+        Invoke("loadGame", delayLoadTime);
+    }
+
+    private void loadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
